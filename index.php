ebecd8ec678f98574e5c85c9b4c3a3715a5e2306
AlenaElagina <Alena-elagina01@yandex.ru>
2020-12-27 20:15:00 +0400

    Шапка сайта

--- index.php
+++ index.php
@@ -7,50 +7,129 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=Lobster&display=swap" rel="stylesheet"> </head>
   
+
+
   <body>
 
-<div class="container">
-  <div class="row align-items-center">
-    <div class="col-4">
-     <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Личный кабинет</button>
-    </div>
-    <div class="col-4"  style="font-family: 'Lobster', cursive;">
-      <h1 style="color:#000099">Топаз </h1>
-    </div>
-    <div class="col-4">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Нравится</button>
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Корзина</button>
-    </div>
+  <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #0000FF;">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" ">
+      <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+              <button href="/pages/lichkab.php" type="button" class="btn btn-outline-light" style="font-family: 'Lobster', cursive;">Личный кабинет</button>
+          </li>
+      </ul>
+      <ul class="navbar-nav">
+          <li class="nav-item my-2 my-lg-0">
+              <button type="button" class="btn btn-outline-light" style="font-family: 'Lobster', cursive;">Нравится</button>
+          </li>
+          <li class="nav-item my-2 my-lg-0">
+              <button type="button" class="btn btn-outline-light" style="font-family: 'Lobster', cursive;">Корзина</button>
+          </li>
+      </ul>
+
+  </nav>
+  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center" style="font-family: 'Lobster', cursive;">
+      <h1 class="display-3" style="color:#000099">Топаз </h1>
   </div>
-  <div class="row">
-  </div>
-</div>
+
 
 <div class="container">
   <div class="row align-items-center">
-    <div class="col">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Серьги</button>
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Кольца</button>
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Подвески</button>
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Браслеты</button>
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Цепочки</button>
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-outline-primary" style="font-family: 'Lobster', cursive;">Броши</button>
+
+      <div class="col">
+          <form action="pages/sergy.php">
+      <button type="button" class="btn btn-outline-primary btn-lg" style="font-family: 'Lobster', cursive;">Серьги</button>
+          </form>
+      </div>
+
+      <div class="col">
+          <form action="pages/kolca.php">
+              <button type="button" class="btn btn-outline-primary btn-lg" style="font-family: 'Lobster', cursive;">Кольца</button>
+          </form>
+      </div>
+
+      <div class="col">
+          <form action="pages/podvesky.php">
+              <button type="button" class="btn btn-outline-primary btn-lg" style="font-family: 'Lobster', cursive;">Подвески</button>
+          </form>
+      </div>
+
+      <div class="col">
+          <form action="pages/braslety.php">
+              <button type="button" class="btn btn-outline-primary btn-lg" style="font-family: 'Lobster', cursive;">Браслеты</button>
+          </form>
+      </div>
+
+      <div class="col">
+          <form action="pages/cepochky.php">
+              <button type="button" class="btn btn-outline-primary btn-lg" style="font-family: 'Lobster', cursive;">Цепочки</button>
+          </form>
+      </div>
+
+      <div class="col">
+          <form action="pages/broshky.php">
+              <button type="button" class="btn btn-outline-primary btn-lg" style="font-family: 'Lobster', cursive;">Броши</button>
+          </form>
     </div>
   </div>
  </div>
+
+<div class="container">
+
+
+    <div class="row justify-content-evenly">
+        <div class="col-4">
+            <button id="close-image"><img src="assets/images/B8.jpg" class="rounded float-left" width="300" height="300"></button>
+        </div>
+        <div class="col-4">
+            <button id="close-image" ><img src="assets/images/BR8.jpg" class="rounded float-left" width="300" height="300"></button>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+  <div class="container marketing">
+      <div class="row">
+          <div class="col">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+              <h2>Heading</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+      </div>
+
+
+
+
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
-  
-  
-  
+
   </body>
 </html>
